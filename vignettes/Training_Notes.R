ee72e54b67f0664348ce8a0d57a381d3a21fda82
jharcum07 <jon.harcum@tetratech.com>
2024-11-13 15:47:45 -0500

    v0.1.0.90305

--- vignettes/Training_Notes.R
+++
@@ -1,9 +0,0 @@
-## ----setup, include = FALSE---------------------------------------------------
-knitr::opts_chunk$set(
-  collapse = TRUE,
-  comment = "#>"
-)
-
-## ----initialize, echo=TRUE, eval=FALSE----------------------------------------
-#  library(tidBIT)
-
